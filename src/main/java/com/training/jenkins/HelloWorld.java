2bdae858c8a5fc905e7e66a401a9c185a47567bc
Rahlasherin@arstraffic.com <Rahlasherin@arstraffic.com>
2023-11-27 16:12:31 +0530

    change content

--- src/main/java/com/training/jenkins/HelloWorld.java
+++ src/main/java/com/training/jenkins/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String args[])
 	{
-		System.out.println(" Welcome to Hello WORLD .....");
+		System.out.println(" Welcome to Hello WORLD .....have a nice day");
 	}
 	
 }
